469cc5f3cc5f6f80d0ea9a669acdb593c8f401cb
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-17 15:06:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "ec2_module" {
-  source = "./modules/ec2-module"
+  source = "./modules/ec2"
   instance_name = "tbdp-bi-ec2"
   instance_type = "t3.micro"
-  ami_id = "ami-0fe65665c4d6f0de7"
+  ami_id = "ami-03cc2fdb1443ab619"
 }
  
 module "dynamodb_module" {
-  source = "./modules/dynamodb_module"
+  source = "./modules/DynamoDB"
   table_name = "tbdp-bi-employee-table"
 }
