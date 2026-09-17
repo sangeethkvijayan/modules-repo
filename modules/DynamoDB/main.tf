25d054eff051cf379f48e88661dee58b47c3c6f5
sangeethkvijayan <sangeethkvijayan9539@gmail.com>
2026-09-17 14:54:20 +0530

    Create main.tf

---
+++ modules/DynamoDB/main.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "dynamodb" {
+ 
+  name         = var.table_name
+ 
+  billing_mode = "PAY_PER_REQUEST"
+ 
+  hash_key = "id"
+ 
+  attribute {
+    name = "id"
+    type = "S"
+  }
+ 
+  tags = {
+    Project = "TBDP-BI DevOps"
+  }
+}
